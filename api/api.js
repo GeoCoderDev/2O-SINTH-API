cb24ec58ce82d62c5f90e25e73023c471128369d
Juanito3500 <juanchavezsaldana1@gmail.com>
2023-12-15 22:22:35 -0500

    Prueba8

--- api/api.js
+++ api/api.js
@@ -3,12 +3,12 @@ const path = require('path');
 const { isAuthenticated } = require(path.join(__dirname, "auth", "index.js"));
 
 // Importing the Routers
-const authRouter = require(path.join(__dirname, "routes", "Authh.js"));
-const usersRouter = require(path.join(__dirname, "routes", "Users.js"));
-const presetsRouter = require(path.join(__dirname, "routes", "Presets.js"));
-const effectsRouter = require(path.join(__dirname, "routes", "Effects.js"));
-const melodiesRouter = require(path.join(__dirname, "routes", "Melodies.js"));
-const rhythmsRouter = require(path.join(__dirname, "routes", "Rhythms.js"));
+const authRouter = require(path.join(__dirname, "routesApi", "Auth.js"));
+const usersRouter = require(path.join(__dirname, "routesApi", "Users.js"));
+const presetsRouter = require(path.join(__dirname, "routesApi", "Presets.js"));
+const effectsRouter = require(path.join(__dirname, "routesApi", "Effects.js"));
+const melodiesRouter = require(path.join(__dirname, "routesApi", "Melodies.js"));
+const rhythmsRouter = require(path.join(__dirname, "routesApi", "Rhythms.js"));
 
 const API = express.Router();
 
